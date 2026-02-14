7411958d343e04a815b3166f268b52ecab31d026
Gabriel PLASSAT <transportsdufutur@gmail.com>
2026-02-14 11:13:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -175,8 +175,7 @@ with tab3:
             "Vecteurs marketing": q11,
             "Informations complémentaires": q12
         }
-        prompt_doc.add_heading("Relire, compléter si besoin et copier/coller ce texte à la fin du fichier de réponse de SofIA", 1)
-        prompt_doc.add_heading("Ce document complet est à relire, compléter, ajuster. Puis il sera fourni à un Assistant Eval (au format .pdf) pour proposer un mode d'intervention. Se connecter à Eval via https://m365.cloud.microsoft/chat/?titleId=T_7b923e69-c9aa-4317-d331-4647b285be26", 1)
+        prompt_doc.add_heading("Ce document complet est à relire, compléter, ajuster. Puis il sera fourni à un Assistant Eval (au format .pdf) pour proposer un mode d'intervention. Se connecter à [Eval](https://m365.cloud.microsoft/chat/?titleId=T_7b923e69-c9aa-4317-d331-4647b285be26) via https://m365.cloud.microsoft/chat/?titleId=T_7b923e69-c9aa-4317-d331-4647b285be26", 1)
 
         
         for key, value in data.items():
